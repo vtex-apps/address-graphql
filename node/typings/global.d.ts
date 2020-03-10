36131e6a04a4144f2eca5d6d0e81ec2738c86bb5
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-10 18:00:52 -0300

    add address and rules global typings

--- node/typings/global.d.ts
+++ node/typings/global.d.ts
@@ -6,6 +6,65 @@ declare global {
   type Context = ServiceContext & {
     clients: Clients
   }
-}
 
-export {}
+  enum AddressType {
+    residential,
+    commercial,
+    inStore,
+    giftRegistry,
+    pickup,
+    search,
+  }
+
+  interface Address {
+    addressId: string
+    addressType: AddressType
+    city: string
+    complement: string
+    country: Strinstring
+    geoCoordinates: number[]
+    neighborhood: string
+    number: string
+    postalCode: string
+    receiverName: string
+    reference: string
+    state: string
+    street: string
+  }
+
+  type AddressFields = keyof Address
+
+  type AddressWithValidation = {
+    [prop in AddressFields]: {
+      value: Address[prop]
+      valid?: boolean
+    }
+  }
+
+  interface Rule {
+    valueIn: 'long_name' | 'short_name'
+    types: string[]
+    required?: boolean
+    notApplicable?: boolean
+    handler?: (
+      address: Address,
+      googleAddress: google.maps.GeocoderResult
+    ) => Address
+  }
+
+  type GeolocationComponents =
+    | 'postalCode'
+    | 'street'
+    | 'neighborhood'
+    | 'state'
+    | 'city'
+    | 'number'
+
+  type GeolocationRules = {
+    [component in GeolocationComponents]?: Rule
+  }
+
+  interface Rules {
+    geolocation: GeolocationRules
+  }
+}
