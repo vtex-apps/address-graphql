454814d98efe1222f49c6984094d231b05f2e97b
Wesley Rocha <wesleysr1997@gmail.com>
2020-08-27 16:20:25 -0400

    Add search resolver for autocomplete queries
    Based on the `geolocation.ts`. Some refactoring were made and it would be cool to make those same refactors on the original `geolocation.ts` file.

--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -1,9 +1,11 @@
 import { queries as geolocationQueries } from './geolocation'
 import { queries as postalCodeQueries } from './postalCode'
+import { queries as searchQueries } from './search'
 
 export const resolvers = {
   Query: {
     ...geolocationQueries,
     ...postalCodeQueries,
+    ...searchQueries,
   },
 }
