512b5b392e7ef88f969e07cc0ece7205cc1e2c29
Wesley Rocha <wesleysr1997@gmail.com>
2020-08-27 16:21:08 -0400

    Format GoogleGeolocation.ts

--- node/clients/GoogleGeolocation.ts
+++ node/clients/GoogleGeolocation.ts
@@ -6,12 +6,9 @@ export class GoogleGeolocation extends ExternalClient {
   }
 
   public reverseGeocode = (lat: string, lng: string, apiKey: string) => {
-    return this.http.get(
-      `/geocode/json?latlng=${lat},${lng}&key=${apiKey}`,
-      {
-        metric: 'google-reverse-geocode-country',
-        timeout: 3000,
-      }
-    )
+    return this.http.get(`/geocode/json?latlng=${lat},${lng}&key=${apiKey}`, {
+      metric: 'google-reverse-geocode-country',
+      timeout: 3000,
+    })
   }
 }
